925d7af1dd15384d8480e2af4645069bfd450691
Pietro <pietro.simonini@hotmail.it>
2019-08-19 13:42:27 +0200

    Revert "Project Cleanup"
    This reverts commit b7fbd78f5e5e29577f5f48721c89dd42efa4aa31.

---
+++ SSMD/Entities/InspectionPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSMD
+{
+    public class InspectionPoint
+    {
+        public InspectionPoint()
+        {
+
+        }
+
+        public int Number { get; set; }
+        public int InspectionOperationNumber { get; set; }
+        public string UM { get; set; }
+        public DateTime InspectionDate { get; set; }
+
+        public InspectionOperation InspectionOperation { get; set; }
+    }
+}
---
+++ SSMD/Entities/OrderConfirmation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSMD
+{
+    public class OrderConfirmation
+    {
+        public OrderConfirmation()
+        {
+
+        }
+
+        public int ConfirmationNumber { get; set; }
+        public int ConfirmationCounter { get; set; }
+        public int InspectionCharacteristicNumber { get; set; }
+
+        public DateTime EntryDate { get; set; }
+    }
+}
