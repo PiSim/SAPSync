4bab1999cbcea4847d66e07db9d7050b47878aa4
Pietro <pietro.simonini@hotmail.it>
2019-08-29 17:33:06 +0200

    Completed DI implementation. SAP connector broke (again)

---
+++ DMTAgent.SAP/RFCFunctions/BAPITableFunctionBase.cs
@@ -0,0 +1,39 @@
+﻿using SAP.Middleware.Connector;
+
+namespace DMTAgent.SAP
+{
+    public class BAPITableFunctionBase
+    {
+        #region Fields
+
+        internal string _functionName,
+                _tableName;
+
+        internal IRfcFunction _rfcFunction;
+
+        #endregion Fields
+
+        #region Methods
+
+        public IRfcTable Invoke(RfcDestination rfcDestination)
+        {
+            InitializeFunction(rfcDestination);
+            _rfcFunction.Invoke(rfcDestination);
+            IRfcTable output = _rfcFunction.GetTable(_tableName);
+            return output;
+        }
+
+        protected virtual void InitializeParameters()
+        {
+        }
+
+        private void InitializeFunction(RfcDestination rfcDestination)
+        {
+            RfcRepository rfcRepository = rfcDestination.Repository;
+            _rfcFunction = rfcRepository.CreateFunction(_functionName);
+            InitializeParameters();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ DMTAgent.SAP/RFCFunctions/ReadRoutingOperations.cs
@@ -0,0 +1,44 @@
+﻿using SSMD;
+
+namespace DMTAgent.SAP
+{
+    public class ReadRoutingOperations : ReadTableBase<RoutingOperation>
+    {
+        #region Constructors
+
+        public ReadRoutingOperations() : base()
+        {
+            _tableName = "AFVC";
+
+            _fields = new string[]
+            {
+                "AUFPL",
+                "APLZL",
+                "ARBID"
+            };
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        internal override RoutingOperation ConvertDataArray(string[] data)
+        {
+            if (!int.TryParse(data[0], out int routingOperationNumber)
+                || !int.TryParse(data[1], out int routingOperationCounter)
+                || !int.TryParse(data[2], out int workCenterID))
+                return null;
+
+            RoutingOperation output = new RoutingOperation()
+            {
+                RoutingNumber = routingOperationNumber,
+                RoutingCounter = routingOperationCounter,
+                WorkCenterID = workCenterID
+            };
+
+            return output;
+        }
+
+        #endregion Methods
+    }
+}
